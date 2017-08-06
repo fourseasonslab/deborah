c2fbf37e52066ecdb6238c57e34e7e6f1385228a
nikosai <nikosai@users.noreply.github.com>
2017-08-06 18:41:20 +0900

    add comments in deborah.ts

--- src/deborah.ts
+++ src/deborah.ts
@@ -1,32 +1,49 @@
+/**
+ * chatbotの本体となるクラス。
+ */
 class Deborah
 {
+	// =============== 変数宣言 ===============
+	/** 利用可能なDriver */
 	driverList: DeborahDriver[] = [];
+	/** 利用可能なResponder */
+	responderList: DeborahResponder[] = [];
+	/** settings.jsonの内容 */
 	settings: any;
+	/** 現在のDeborahMemory */
+	memory: DeborahMemory;
+	/** MeCabのインスタンス */
 	mecab: any;
+	/** Cabochaのインスタンス */
 	//cabochaf0: any;
 	cabochaf1: any;
+	/** 起動時刻 */
 	launchDate: Date;
-	initialIgnorePeriod: number = 5000;	// ms
-	fixedResponseList: (string[])[] = [
-		[":fish_cake:", "やっぱなるとだよね！ :fish_cake:"],
-		["むり", "まあまあ。:zabuton: 一休みですよ！ :sleeping:"],
-		["死", "まだ死ぬには早いですよ！ :iconv:"],
-		["test","test"]
-	];
-	responderList: DeborahResponder[] = [];
-	memory: DeborahMemory;
-	Cabocha: any;
+	// /** 起動時に入ってくるメッセージを無視する時間[ms]（no longer used?） */
+	// initialIgnorePeriod: number = 5000;
+	// /** 定型文での返答パターン（no longer used?） */
+	// fixedResponseList: (string[])[] = [
+	// 	[":fish_cake:", "やっぱなるとだよね！ :fish_cake:"],
+	// 	["むり", "まあまあ。:zabuton: 一休みですよ！ :sleeping:"],
+	// 	["死", "まだ死ぬには早いですよ！ :iconv:"],
+	// 	["test","test"]
+	// ];
+
+	/**
+	 * コンストラクタ。各種変数の初期化を担当。
+	 */
 	constructor(){
 		console.log("Initializing deborah...");
-		//
-		this.Cabocha = require('node-cabocha');
+		
+		// =============== Cabocha ===============
+		var Cabocha = require('node-cabocha');
 		try{
-			this.cabochaf1 = new this.Cabocha();
+			this.cabochaf1 = new Cabocha();
 		} catch(e){
 			console.error(e);
 		}
-		//
-		this.launchDate = new Date();
+		
+		// =============== Load Settings ===============
 		var fs = require("fs");
 		let fval, fname = "settings.json";
 		try {
@@ -40,6 +57,9 @@ class Deborah
 			process.exit(1);
 		}
 		this.settings = JSON.parse(fval);
+
+		// =============== others ===============
+		this.launchDate = new Date();
 		//console.log(JSON.stringify(this.settings, null, 1));
 		this.memory = new DeborahMemory("memory.json");
 		var MeCab = require('mecab-lite');
@@ -51,12 +71,19 @@ class Deborah
 		//this.responderList.push(new DeborahResponderMeCab(this));
 		this.responderList.push(new DeborahResponderMemory(this));
 	}
+
+	/**
+	 * 起動時に呼ばれる関数。Driver関係の処理を行う。
+	 */
 	start(){
+		// =============== Load Settings ===============
 		var interfaces = this.settings.interfaces;
 		if (!(interfaces instanceof Array)) {
 			console.log("settings.interfaces is not an Array.");
 			process.exit(0);
 		}
+
+		// Settingsのinterfacesに対応するDriverをDriverListに追加
 		for (var i = 0; i < interfaces.length; i++) {
 			var iset = interfaces[i];
 			if (iset.type == "slack-connection") {
@@ -72,6 +99,11 @@ class Deborah
 			}
 		}
 	}
+
+	/**
+	 * メッセージを受け取った場合の動作。
+	 * @param data 受け取ったメッセージ
+	 */
 	receive(data: DeborahMessage){
 		try {
 			// メッセージが空なら帰る
@@ -80,14 +112,14 @@ class Deborah
 			// 記憶に追加
 			this.memory.appendReceiveHistory(data);
 
-			//この下4行はanalyzeに食べさせた結果を使うresponders用
+			// この下4行はanalyzeに食べさせた結果を使うresponders用
 			var that = this;
 			data.analyze(function(data2: DeborahMessage){
 				if(that.responderList.length > 0){
 					// ランダムにresponderを選択して、それに処理を引き渡す。
 					var idx = Math.floor(Math.random() * that.responderList.length);
 					console.log("Responder: " + that.responderList[idx].name);
-					that.responderList[idx].generateResponse(data); 
+					that.responderList[idx].generateResponse(data);
 				} else{
 					console.log("No responder available.");
 				}
@@ -99,12 +131,30 @@ class Deborah
 			console.log("内部エラーが発生しました。\nメッセージ: " + e);
 		}
 	}
+	/**
+	 * <p>%から始まる文字列をコマンドとして認識して対応する処理を行う<br>
+	 * 現在実装済みのコマンドは以下の通り。</p>
+	 * <ul>
+	 * 	<li>%date : 起動時刻を返します</li>
+	 * 	<li>%uptime : 起動からの経過時間[ms]を返します</li>
+	 *	<li>%hello : 挨拶します</li>
+	 * 	<li>%say:  指定の文字列を喋ります</li>
+	 * 	<li>%mecab str : mecabに文字列strを渡して分かち書きの結果を返します</li>
+	 * 	<li>%debug : デバッグ用コマンド。</li>
+	 * 	<li>%history 指定の文字列を喋ります</li>
+	 * 	<li>%say 指定の文字列を喋ります</li>
+	 * 	<li>%say 指定の文字列を喋ります</li>
+	 * </ul>
+	 * @param data 受け取ったメッセージ
+	 */
 	doCommand(data: DeborahMessage){
 		// %から始まる文字列をコマンドとして認識する
 		if (data.text.charAt(0) !== '%') return;
-		var command = data.text.substring(1).split(' ');
+		var args = data.text.substring(1).split(' ');
+		var command = args[0];
+
 		// コマンドの種類により異なる動作を選択
-		switch (command[0].toLowerCase()) {
+		switch (command.toLowerCase()) {
 			case 'date':
 				// %date
 				// 起動時刻を返します
@@ -112,7 +162,7 @@ class Deborah
 				break;
 			case 'uptime':
 				// %uptime
-				// 起動からの経過時間[ms]を返します。
+				// 起動からの経過時間[ms]を返します
 				data.driver.reply(data, "起動してから" + (Date.now() - this.launchDate.getTime()) + "ms経過しました。");
 				break;
 			case 'hello':
@@ -128,9 +178,8 @@ class Deborah
 				break;
 			case 'mecab':
 				// %mecab str
-				// mecabに指定の文字列を渡して分かち書きの結果を返します
+				// mecabに文字列strを渡して分かち書きの結果を返します
 				var str = data.text.split('%mecab ')[1];
-				var that = this;
 				this.mecab.parse(str, function(err, result) {
 					var ans = "";
 					if (result) {
@@ -147,15 +196,19 @@ class Deborah
 				// %debug
 				// デバッグ用コマンド。
 				switch (command[1]){
-					case 'slackData':
+					// rawDataを表示
+					case 'rawData':
 						console.log(data.rawData);
 						break;
-					case 'cur':
+					// this.dataを表示
+					case 'curData':
 						console.log(data);
 						break;
 				}
 				break;
 			case 'history':
+				// %history
+				// 
 				var args = data.text.split('%history')[1].split(" ");
 				var count = Number(args[1]);
 				var sender = args[2];
@@ -171,6 +224,6 @@ class Deborah
 	}
 }
 
+// ここでDeborahのインスタンスを作り（暗黙にコンストラクタが呼ばれる）、さらにstart関数を呼ぶ。
 var deborah = new Deborah();
 deborah.start();
-
