4febf2a27af44808461fbb3674ac6ad4359502b1
nikosai <nikosai@users.noreply.github.com>
2017-08-06 23:52:27 +0900

    add comments for twitter.ts

--- src/driver/twitter.ts
+++ src/driver/twitter.ts
@@ -1,11 +1,26 @@
+/**
+ * Twitterを担当するドライバ
+ */
 class DeborahDriverTwitter extends DeborahDriver
 {
+	/** 生成元であるDeborahのインスタンス */
 	bot: Deborah;
+	/** settings.jsonで与えられたinterfaceの設定 */
 	settings: any;
+	/** Twitterのインスタンス */
 	client: any;
+
+	/**
+	 * コンストラクタ。
+	 * @param bot 生成元であるDeborahのインスタンス
+	 * @param settings settings.jsonで与えられたinterfaceの設定
+	 */
 	constructor(bot: Deborah, settings: any){
+		// 親クラスのコンストラクタを呼ぶ
 		super(bot, settings);
 		console.log("Driver initialized: Twitter");
+
+		// =============== 変数初期化 ===============
 		var Twitter = require('twitter');
 		this.client = new Twitter({
 			consumer_key: settings.consumer_key,
@@ -13,18 +28,32 @@ class DeborahDriverTwitter extends DeborahDriver
 			access_token_key: settings.access_token_key,
 			access_token_secret: settings.access_token_secret
 		});
+
+		// connect関数に処理を引き渡す
+		this.connect();
+	}
+	/**
+	 * 接続を確立する。ユーザータイムラインを監視する。
+	 */
+	connect(){
 		var that = this;
 		this.client.stream('user', function(stream) {
 			stream.on('data', function(data) {
+				/** screen_name （@username なら 「username」） */
 				var id        = ('user' in data && 'screen_name' in data.user) ? data.user.screen_name : null;
+				/** 自分への@リプライを取り除いた、届いたメッセージの本文 */
 				var text      = ('text' in data) ? data.text.replace(new RegExp('^@' + that.settings.screen_name + ' '), '') : '';
-				var ifMention = ('in_reply_to_user_id' in data) ? (data.in_reply_to_user_id !== null) : false;
-				var ifMentionToMe = ifMention && (data.in_reply_to_screen_name === that.settings.screen_name);
+				/** 届いたメッセージがリプライであるか */
+				var isMention = ('in_reply_to_user_id' in data) ? (data.in_reply_to_user_id !== null) : false;
+				/** 届いたメッセージが自分へのリプライであるか */
+				var isMentionToMe = isMention && (data.in_reply_to_screen_name === that.settings.screen_name);
 
 				console.log(data);
 
-				if (!ifMentionToMe || id == that.settings.screen_name) return;
+				// 自分へのリプライで、かつ送信者が自分でないもののみをreceiveする
+				if (!isMentionToMe || id == that.settings.screen_name) return;
 
+				// 受信したメッセージの情報をDeborahMessageに渡す
 				var m = new DeborahMessage();
 				m.text = text;
 				m.senderName = id;
@@ -32,10 +61,17 @@ class DeborahDriverTwitter extends DeborahDriver
 				m.driver = that;
 				m.rawData = data;
 
+				// DeborahにMessageを渡す
 				that.bot.receive(m);
 			});
 		});
 	}
+	
+	/**
+	 * 送られてきたメッセージに返信する
+	 * @param replyTo 返信先となる（送られてきた）メッセージ
+	 * @param message 返信用メッセージ本文
+	 */
 	reply(replyTo: DeborahMessage, message: string){
 		var msg = {
 			"status": "@"+replyTo.senderName+" "+message,
