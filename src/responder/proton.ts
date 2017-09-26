86576750ed6e0ad38c9466e0075a7606e73e6fc9
YokoKato <proton@MacBookProton.local>
2017-09-26 18:18:36 +0900

    making sth like eliza

--- src/responder/proton.ts
+++ src/responder/proton.ts
@@ -1,101 +1,60 @@
 class DeborahResponderProton extends DeborahResponder
 {
-
-	jisho: string[];
-	used_words: string[];
-
 	constructor(bot: Deborah){
 		super(bot);
-		this.name ="Proton";
-		//console.log("計算できたよ！");
-		//
-		const fs = require('fs');
-		this.jisho = ("" + fs.readFileSync('src/responder/yomi.txt')) .split("\n");
-		this.used_words = [];
-
+		this.name = "Proton";
+		//	var eliza = new Elizabot();
 	};
 	generateResponse(req: DeborahMessage){
 
 		var result = req.analytics;
-		//	var that = this;
-		//console.log(req.analytics.words[0][8]);
-		var reply = "";
-		for(var i=0; i<req.analytics.words.length; i++){
-			reply += req.analytics.words[i][8];
+		var that = this;
+		var key_num = 0;
+		var subkey_num = 0;
+		//console.log(result);
+		//まず、入力文の中の単語のうち一番優先度(keylist)が高いものを探す
+		for(var i=0; i<result.words.length; i++){
+			//if(key_num>0) break;
+			for(var j=1; j<probot_keywords.length; j++){//j=0は例外なので1から
+				if(result.words[i][7] == probot_keywords[j][0]){
+
+						//優先度をみる
+						if(probot_keywords[key_num][1]<=probot_keywords[j][1]){
+							key_num = j;
+						}
+
+					//console.log(probot_keywords[j][2][0][1][0]);
+					//var rnd = Math.floor(Math.random() * probot_keywords[j][2][0][1].length);
+					//req.driver.reply(req, probot_keywords[j][2][0][1][rnd]);
+				}
+			}
+			//console.log(result.words[i][7]);//終止形またはそのまま
 		}
-		console.log(reply);
-
-		if(this.used_words.indexOf(reply)>=0){
-			req.driver.reply(req,"それさっき言ったよ〜！あなたの負けね！ふふふ");
+		if(key_num == 0){
+			req.driver.reply(req, probot_keywords[0][2][0][1][0]);
 		}else{
-
-			//履歴に追加
-			this.used_words.push(reply);
-			/*	var jisho = ["カクテル", "リンゴ", "ゴリラ", "ラクダ", 
-			"ダチョウ", "ウミ", "ミント", "トウモコロシ", "シルク", "クルマ",
-			"マイタケ", "ケトル", "ルーマニア", "アメリカ", "カラス",
-			"にほんしゆ", "ゆき"];
-			 */
-
-
-			/*req.driver.reply(req, "最初の文字は");
-			req.driver.reply(req, req.analytics.words[0][8][0]);
-			req.driver.reply(req, ", 最後の文字は");
-			req.driver.reply(req, req.analytics.words[0][8][req.analytics.words[0][8].length - 1]);
-			 */
-
-
-			//最後の文字の例外処理
-			//var last = req.analytics.words[0][8][req.analytics.words[0][8].length - 1];
-			var last = reply[reply.length-1];
-			if(last == "ー"){
-				last = reply[reply.length-2];
-			}
-
-			switch(last){
-				case "ッ":
-					last = "ツ";
-					break;
-				case "ャ":
-					last = "ヤ";
-					break;
-				case "ュ":
-					last = "ユ";
-					break;
-				case "ョ":
-					last = "ヨ";
-					break;
-			}
-
-			if(last == "ン"){
-				req.driver.reply(req, "きさまの負けだ！！！");
-			}else{
-				for(var i=0; i<this.jisho.length; i++){
-					if((last == this.jisho[i][0]) && (this.jisho[i][this.jisho[i].length-1] != "ン")){
-						if(this.used_words.indexOf(this.jisho[i]) < 0){	
-							req.driver.reply(req, this.jisho[i]);
-							this.used_words.push(this.jisho[i]);
-							break;
+			//サブキーが設定されてる場合
+			/*			if(probot_keywords[key_num][2].length > 0){
+				//probot_keywords[key_num][2][!!!][1][rnd]を返す。!!!の数字を判定する。
+				for(var i=0; i<result.words.length; i++){
+					for(var j=1; j<probot_keywords[key_num][2].length; j++){//最初のひとつは例外
+						if(result.words[i][7] == probot_keywords[key_num][2][j][0]){
+							subkey_num = j;
 						}
 					}
 				}
 			}
-			//console.log(this.used_words);
-			/*
-		var rep1 = "";
-		var rep2 = "";
-
-		for(var i = 0; i < req.text.length; i++){
-			if(i%2 == 0){
-				rep1 += req.text[i];
-			}else{
-				rep2 += req.text[i];
-			}
-		}
-
-		req.driver.reply(req, rep1);
-		req.driver.reply(req, rep2);
 			 */
+			//そうでないならsubkey_numは0のまま
+			//ランダムに選択
+			var rnd = Math.floor(Math.random() * probot_keywords[key_num][2][subkey_num][1].length);
+			req.driver.reply(req, probot_keywords[key_num][2][subkey_num][1][rnd]);
+			
 		}
+		//みつけた単語の返答からランダムで返答を選択する
+
+		//ほんとはこんなかんじにしたいけど
+		//req.driver.reply(req, eliza.transform(result));
+	
 	}
 }
